c4479748c9f8975a78bc88b6523282fd56e0770b
ramintheredmn <redcrossprog@gmail.com>
2024-04-08 15:03:44 +0330

    parallel input

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
 import subprocess
 import os
-import time
+from concurrent.futures import ProcessPoolExecutor
+
+def run_autodock_for_directory(directory):
+    base_directory = "."
+    os.chdir(os.path.join(base_directory, directory))
+    print(f"Running autogrid and autodock for {directory}")
+    subprocess.run("autogrid4 -p r.gpf -l r.glg", shell=True, check=True)
+    
+    if os.path.exists('r.glg'):
+        print(f"'r.glg' created in {directory}")
+        subprocess.run("autodock4 -p r.dpf -l r.dlg", shell=True, check=True)
+        print(f"Autodock finished for {directory}")
+    else:
+        print(f"Error: 'r.glg' not created in {directory}")
+
+def run(n):
+    base_directory = "." 
+    directories = [d for d in os.listdir(base_directory) if d.startswith('dock_') and os.path.isdir(os.path.join(base_directory, d))]
+    
+    # Process directories in batches
+    for i in range(0, len(directories), n):
+        batch = directories[i:i+n]
+        with ProcessPoolExecutor(max_workers=n) as executor:
+            executor.map(run_autodock_for_directory, batch)
 
 def main():
-    # assuming you place this script where your dock_* folders are
-    base_directory = "." 
-    # create a list of availible dock_* folders to iterate 
-    directories = [d for d in os.listdir(base_directory) if d.startswith('dock_') and os.path.isdir(os.path.join(base_directory, d))]
-    # print(directories)
-    for directory in directories:
-        print(f"runing autofird for {directory} ")
-        # Change the current working directory to 'directory'
-        os.chdir(os.path.join(base_directory, directory))
-        print("Current working directory:", os.getcwd())
-        # Run autogrid
-        # first place to change if you have diffetent file names
-        process_grid = subprocess.Popen("autogrid4 -p r.gpf -l r.glg", shell=True)
-        
-        # Wait for autogrid to complete
-        process_grid.wait()
-
-        # Check if 'r.glg' is created
-        if os.path.exists('r.glg'):
-            print(f"'r.glg' created in {directory}")
-            print("Current working directory:", os.getcwd())
-            # now starting the docking process
-            # second place to change if you have different file names
-            process_dock = subprocess.Popen("autodock4 -p r.dpf -l r.dlg", shell=True)
-            process_dock.wait()
-            print(f'autodock finished for {directory}')
-        
-        # Change back to the base directory
-        os.chdir(base_directory)
-        print("Current working directory:", os.getcwd())
-        os.chdir("..")
-
+    s_run_number = int(input("Enter the number of parallel runs, consider your CPU cores do not exceed n-1, where n is the number of cores: "))
+    run(s_run_number)
 
 if __name__ == "__main__":
     main()
+
